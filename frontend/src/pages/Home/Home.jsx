4859abd062cde58ee2644f36149f1954dc1b7e06
nguyennhan2006 <88951683+nguyennhan2006@users.noreply.github.com>
2025-04-15 13:45:19 +0700

    Prepare Homepage

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Rules from './rules';
 
 function Home() {
   return (
     <div>
       <h1 className='text-3xl bg-red-500'>Trang chủ</h1>
       <p>Đây là trang chủ của ứng dụng.</p>
-      
+      <Rules/>
     </div>
+    
   );
 }
 
