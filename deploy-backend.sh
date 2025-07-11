46cecc35272858d79eaeee2ef4cf20f70d40e749
Khoa Cao <dkhoacao2906@gmail.com>
2025-07-12 02:27:29 +0700

    Optimize backend deployment to skip unnecessary restarts
    The deploy-backend.sh script now checks if there are code changes before restarting the Docker container. If the code is already up to date, the script skips the container restart, reducing downtime and unnecessary operations.

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 cd /home/dkhoa2906/apps/ucpc-2025-website/backend || exit
-git pull origin main
-docker-compose down
-docker-compose up -d --build
+PULL_OUTPUT=$(git pull origin main)
+
+if [[ "$PULL_OUTPUT" != "Already up to date." ]]; then
+  echo "Code changed. Restarting container..."
+  docker-compose down
+  docker-compose up -d --build
+else
+  echo "No change. Skipping restart."
+fi
